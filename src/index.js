de109a56571a897605a5d59fb8ce91a517f1a4ad
whitemarble <wenhan@outlook.com>
2017-09-04 15:38:21 +1200

    ...

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
 import {createStore} from 'redux';
 import myApp from './reducers';
+
+import App from './App';
 
 
 let store = createStore(myApp);
